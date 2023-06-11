cf2860feb1ef08f07ac42f41336d6c820a70ece9
Adnane bouram <bouramadnane@gmail.com>
2023-06-11 15:33:02 +0100

    hotbar functionality

--- GenerateTiles.gd
+++ GenerateTiles.gd
@@ -4,14 +4,64 @@ extends Node
 var tile = preload("res://tile.tscn")
 @export var size:Vector2
 @export var bounds:Vector2
-@export var padding:int
+@export var padding:float
+var tiles:Array
 func _ready():
 	for i in size.x:
 		for j in size.y:
 			var tileinst = tile.instantiate()
-			var spawnpos = Vector2(i,j)*bounds + Vector2(padding,padding)
+			var spawnpos = (Vector2(i,j))*(bounds * padding)
 			add_child(tileinst)
 			tileinst.transform.origin = spawnpos
+			tiles.append(tileinst)
 			#get_tree().root.add_child()
 	pass # Replace with function body.
 
+
+
+
+func _on_test_1_pressed():
+	for t in tiles:
+		if t.selected:
+			t.type = "test"
+	pass # Replace with function body.
+
+
+func _on_bar_pressed():
+	for t in tiles:
+		if t.selected:
+			t.type = "bar"
+	pass # Replace with function body.
+
+
+func _on_advertisement_pressed():
+	for t in tiles:
+		if t.selected:
+			t.type = "advertisement"
+	pass # Replace with function body.
+
+
+func _on_voleyball_pressed():
+	for t in tiles:
+		if t.selected:
+			t.type = "volleyball"
+	pass # Replace with function body.
+
+
+func _on_pharmacy_pressed():
+	for t in tiles:
+		if t.selected:
+			t.type = "pharmacy"
+	pass # Replace with function body.
+
+
+func _on_desect_pressed():
+	for t in tiles:
+		t.selected = false
+	pass # Replace with function body.
+
+
+func _on_inverse_pressed():
+	for t in tiles:
+		t.selected = not t.selected
+	pass # Replace with function body.
--- tile.gd
+++ tile.gd
@@ -1,12 +1,23 @@
 extends Sprite2D
+class_name tile
+@export var selectedc:Color
+@export var notselectedc:Color
+@export var type:String
 
+@onready var label = $Label
 
-#@export var debug:Color
+var selected:bool
 
+func _ready():
+	label.text = ""
+
+func _process(delta):
+	modulate = selectedc if selected else notselectedc	
+	label.text = type
 # Called when the node enters the scene tree for the first time.
 func _input(event):
-	if event.is_action("click"):
+	if event.is_action("select"):
 		if event.is_pressed():
 			if get_rect().has_point(get_local_mouse_position()):
-				#modulate = debug
+				selected = not selected
 				pass
