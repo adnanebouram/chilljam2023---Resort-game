f045b35ff6865ede90528f06331b540fb5289841
Adnane bouram <bouramadnane@gmail.com>
2023-06-11 13:41:48 +0100

    added an npc and some animations

--- tile.gd
+++ tile.gd
@@ -1,11 +1,12 @@
 extends Sprite2D
 
 
-@export var debug:Color
+#@export var debug:Color
 
 # Called when the node enters the scene tree for the first time.
 func _input(event):
 	if event.is_action("click"):
 		if event.is_pressed():
 			if get_rect().has_point(get_local_mouse_position()):
-				modulate = debug
+				#modulate = debug
+				pass
