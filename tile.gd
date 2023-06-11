a4b6987a0472aa58837af93f3d84db2b47eb44fb
Adnane bouram <bouramadnane@gmail.com>
2023-06-11 13:25:47 +0100

    Clicking Bug
    The wrong tile detects clicks

--- tile.gd
+++ tile.gd
@@ -1,8 +1,11 @@
 extends Sprite2D
 
 
+@export var debug:Color
+
 # Called when the node enters the scene tree for the first time.
 func _input(event):
 	if event.is_action("click"):
 		if event.is_pressed():
-			pass
+			if get_rect().has_point(to_local(event.position)):
+				modulate = debug
